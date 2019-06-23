0d875b29a8db6ea588eaefb248ac68f6d9cc5f80
huangshuai <young921006@gmail.com>
2019-06-23 15:50:42 +0800

    init

---
+++ src/main/java/com/hs/hornet/wormhole/WormholeApplication.java
@@ -0,0 +1,13 @@
+package com.hs.hornet.wormhole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WormholeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WormholeApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hs/hornet/wormhole/internal/RequestField.java
@@ -0,0 +1,13 @@
+package com.hs.hornet.wormhole.internal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface RequestField {
+
+    public String Value();
+}
